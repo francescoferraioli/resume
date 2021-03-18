294172ac39e3c1f3621134e1527d93976016f1af
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2021-03-18 22:00:17 +1000

    Use handlebars instead of mustache

--- build/build.ts
+++ build/build.ts
@@ -1,11 +1,15 @@
 import * as fs from "fs";
-import * as Mustache from "mustache";
-import { view } from "./view";
+import * as Handlebars from "handlebars";
+import { data } from "./data";
 
-const src = fs.readFileSync("src/index.mst", "utf8");
-
-const dist = Mustache.render(src, view, (partial) =>
-  fs.readFileSync(`src/partials/${partial}.mst`, "utf8")
+Handlebars.registerPartial("partial", (context) =>
+  Handlebars.compile(
+    fs.readFileSync(`src/partials/${context.partial}.hbs`, "utf8")
+  )(context)
 );
+
+const src = fs.readFileSync("src/index.hbs", "utf8");
+
+const dist = Handlebars.compile(src)(data);
 
 fs.writeFileSync("docs/index.html", dist);
---
+++ build/data.ts
@@ -0,0 +1,9 @@
+export interface Data {
+  firstName: string;
+  lastName: string;
+}
+
+export const data: Data = {
+  firstName: "Francesco",
+  lastName: "Ferraioli",
+};
