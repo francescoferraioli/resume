ab77c4e95bfa17191bc2534252acd85d28581bc0
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2021-03-18 19:32:00 +1000

    Use mustache file extension

--- build/build.ts
+++ build/build.ts
@@ -2,7 +2,7 @@ import * as fs from "fs";
 import * as Mustache from "mustache";
 import { view } from "./view";
 
-const src = fs.readFileSync("src/index.html", "utf8");
+const src = fs.readFileSync("src/index.mustache", "utf8");
 
 const dist = Mustache.render(src, view);
 
