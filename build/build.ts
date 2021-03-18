51feb551cbcde62c48b8f0a1eece68e7543f6e44
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2021-03-18 17:47:12 +1000

    Introduce mustache view

--- build/build.ts
+++ build/build.ts
@@ -1,7 +1,8 @@
 import * as fs from "fs";
 import * as Mustache from "mustache";
+import { view } from "./view";
 const src = fs.readFileSync("src/index.html", "utf8");
 
-const dist = Mustache.render(src, {});
+const dist = Mustache.render(src, view);
 
 fs.writeFileSync("docs/index.html", dist);
---
+++ build/view.ts
@@ -0,0 +1,9 @@
+export interface View {
+  firstName: string;
+  lastName: string;
+}
+
+export const view: View = {
+  firstName: "Francesco",
+  lastName: "Ferraioli",
+};
