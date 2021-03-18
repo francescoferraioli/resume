97a633e129b640eeddd015c97909902924645866
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2021-03-18 19:53:17 +1000

    Use mst instead of mustache extension

--- build/build.ts
+++ build/build.ts
@@ -2,10 +2,10 @@ import * as fs from "fs";
 import * as Mustache from "mustache";
 import { view } from "./view";
 
-const src = fs.readFileSync("src/index.mustache", "utf8");
+const src = fs.readFileSync("src/index.mst", "utf8");
 
 const dist = Mustache.render(src, view, (partial) =>
-  fs.readFileSync(`src/partials/${partial}.mustache`, "utf8")
+  fs.readFileSync(`src/partials/${partial}.mst`, "utf8")
 );
 
 fs.writeFileSync("docs/index.html", dist);
