29d3a18e00728a4489eea682fc2e8cc88d18432f
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2021-03-18 19:49:35 +1000

    Add support for partials

--- build/build.ts
+++ build/build.ts
@@ -4,6 +4,8 @@ import { view } from "./view";
 
 const src = fs.readFileSync("src/index.mustache", "utf8");
 
-const dist = Mustache.render(src, view);
+const dist = Mustache.render(src, view, (partial) =>
+  fs.readFileSync(`src/partials/${partial}.mustache`, "utf8")
+);
 
 fs.writeFileSync("docs/index.html", dist);
