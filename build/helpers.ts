9bfa972e4ac36f4ce1984c66116339900235b243
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2021-03-18 22:54:10 +1000

    Extract pages into a page partial

--- build/helpers.ts
+++ build/helpers.ts
@@ -2,6 +2,7 @@ import { HelperDeclareSpec } from "handlebars";
 
 const helpers: HelperDeclareSpec = {
   uppercase: (x: string) => x.toUpperCase(),
+  pagePartial: (x: string) => `pages/${x}`,
 };
 
 export default helpers;
