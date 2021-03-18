d5246e48d73f5f6553aee1872bf557c359305a7a
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2021-03-18 19:50:09 +1000

    Add and user the footer partial

--- build/view.ts
+++ build/view.ts
@@ -1,9 +1,25 @@
 export interface View {
   firstName: string;
   lastName: string;
+  pageMeta: {
+    1: {
+      num: 1;
+    };
+    2: {
+      num: 2;
+    };
+  };
 }
 
 export const view: View = {
   firstName: "Francesco",
   lastName: "Ferraioli",
+  pageMeta: {
+    1: {
+      num: 1,
+    },
+    2: {
+      num: 2,
+    },
+  },
 };
