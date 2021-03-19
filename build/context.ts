680ac2b6f48257a9361cbf53f09112b3714ff87f
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2021-03-19 18:25:02 +1000

    Abstract away the section html

--- build/context.ts
+++ build/context.ts
@@ -1,11 +1,42 @@
 export interface Context {
   firstName: string;
   lastName: string;
+  sections: Sections;
+}
+
+interface Sections {
+  summary: Section;
+  skills: Section;
+  interests: Section;
+  experience: Section;
+}
+
+interface Section {
+  name: string;
+  icon: string;
 }
 
 export const getContext = (): Context => {
   return {
     firstName: "Francesco",
     lastName: "Ferraioli",
+    sections: {
+      summary: {
+        name: "summary",
+        icon: "bars",
+      },
+      skills: {
+        name: "skills",
+        icon: "wrench",
+      },
+      interests: {
+        name: "interests",
+        icon: "heart",
+      },
+      experience: {
+        name: "experience",
+        icon: "briefcase",
+      },
+    },
   };
 };
--- build/helpers.ts
+++ build/helpers.ts
@@ -3,6 +3,7 @@ import { HelperDeclareSpec } from "handlebars";
 const helpers: HelperDeclareSpec = {
   uppercase: (x: string) => x.toUpperCase(),
   pagePartial: (x: string) => `pages/${x}`,
+  sectionPartial: (x: string) => `sections/${x}`,
 };
 
 export default helpers;
