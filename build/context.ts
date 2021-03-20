2f60ac1162ed2187f7451dab06bde6d0cd56300e
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2021-03-20 16:16:21 +1000

    Put contactInfo into the context

--- build/context.ts
+++ build/context.ts
@@ -1,7 +1,16 @@
 export interface Context {
   firstName: string;
   lastName: string;
+  contactInfo: ContactInfo;
   sections: Sections;
+}
+
+interface ContactInfo {
+  address: string;
+  phone: string;
+  email: string;
+  github: string;
+  linkedIn: string;
 }
 
 interface Sections {
@@ -20,6 +29,13 @@ export const getContext = (): Context => {
   return {
     firstName: "Francesco",
     lastName: "Ferraioli",
+    contactInfo: {
+      address: "Brisbane",
+      phone: "424938538",
+      email: "francesco.ferraioli@outlook.com",
+      github: "francescoferraioli",
+      linkedIn: "ferraiolifrancesco",
+    },
     sections: {
       summary: {
         name: "summary",
--- build/helpers.ts
+++ build/helpers.ts
@@ -4,6 +4,10 @@ const helpers: HelperDeclareSpec = {
   uppercase: (x: string) => x.toUpperCase(),
   pagePartial: (x: string) => `pages/${x}`,
   sectionPartial: (x: string) => `sections/${x}`,
+  formatPhone: (x: string) => {
+    const [, first, second, third] = x.match(/^(\d{3})(\d{3})(\d{3})$/);
+    return `0${first} ${second} ${third}`;
+  },
 };
 
 export default helpers;
