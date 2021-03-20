4eb090953b0d4068804a4f54c0f03ef51ea3751a
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2021-03-20 16:37:59 +1000

    Move the summary in the context

--- build/context.ts
+++ build/context.ts
@@ -3,6 +3,7 @@ export interface Context {
   lastName: string;
   contactInfo: ContactInfo;
   sections: Sections;
+  summary: string[];
 }
 
 interface ContactInfo {
@@ -54,5 +55,13 @@ export const getContext = (): Context => {
         icon: "briefcase",
       },
     },
+    summary,
   };
 };
+
+const summary: string[] = [
+  "I am an extremely passionate full stack engineer favoring .NET (C# / F#) on the backend and React/Angular on the front end whilst having a broad experience in many other technologies.",
+  "I am in love with using functional programming patterns to create intelligent, fast, secure, maintainable, readable, testable and reliable systems.",
+  "I am a strong believer in being in a continuous state of learning as technology continues to rapidly evolve.",
+  "I also believe that working in a team of people with the same passion is paramount.",
+];
