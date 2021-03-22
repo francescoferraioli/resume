6f87b44882c0cb682769e349a09770ab0752f5ed
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2021-03-23 07:33:34 +1000

    Add switch/case helpers

--- build/helpers.ts
+++ build/helpers.ts
@@ -17,6 +17,15 @@ const helpers: HelperDeclareSpec = {
       </div>`
     );
   },
+  switch: function (value, options) {
+    this.switch_value = value;
+    return options.fn(this);
+  },
+  case: function (value, options) {
+    if (value == this.switch_value) {
+      return options.fn(this);
+    }
+  },
 };
 
 export default helpers;
