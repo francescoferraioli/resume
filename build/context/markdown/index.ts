d379c87034702f93da4ef20f4b67bb8aa1c14efc
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2021-03-24 07:05:26 +1000

    Rearrange markdown ts file

--- build/context/markdown/index.ts
+++ build/context/markdown/index.ts
@@ -7,37 +7,6 @@ import {
 } from "./instructions";
 import { getRendererForText } from "./renderers";
 
-const markdownFolder = "src/md/";
-
-const mapToMarkDownLine = (line: string): MarkDownLine => {
-  if (isInstruction(line)) {
-    return parseInstruction(line);
-  }
-
-  return {
-    type: "text",
-    line,
-  };
-};
-
-const parseMarkdownFile = (file: string): MarkDownRendered[] => {
-  return fs
-    .readFileSync(file, "utf-8")
-    .split("\n")
-    .map(mapToMarkDownLine)
-    .reduce((acc, line) => {
-      switch (line.type) {
-        case "text":
-          return [...acc, getRendererForText(line.line).render()];
-        case "instruction": {
-          return acc;
-        }
-        default:
-          assertUnreachable(line);
-      }
-    }, []);
-};
-
 export type MarkDownRendered = MarkDownStandard | MarkDownHtml | MarkDownSpacer;
 
 interface MarkDownStandard {
@@ -62,11 +31,42 @@ interface MarkDownText {
   line: string;
 }
 
+const markdownFolder = "src/md/";
+
 const buildMarkdownForFile = (
   name: string
 ): Record<string, MarkDownRendered[]> => ({
   [name.replace(".md", "")]: parseMarkdownFile(`${markdownFolder}${name}`),
 });
+
+const parseMarkdownFile = (file: string): MarkDownRendered[] => {
+  return fs
+    .readFileSync(file, "utf-8")
+    .split("\n")
+    .map(mapToMarkDownLine)
+    .reduce((acc, line) => {
+      switch (line.type) {
+        case "text":
+          return [...acc, getRendererForText(line.line).render()];
+        case "instruction": {
+          return acc;
+        }
+        default:
+          assertUnreachable(line);
+      }
+    }, []);
+};
+
+const mapToMarkDownLine = (line: string): MarkDownLine => {
+  if (isInstruction(line)) {
+    return parseInstruction(line);
+  }
+
+  return {
+    type: "text",
+    line,
+  };
+};
 
 export const markdown: Record<string, MarkDownRendered[]> = fs
   .readdirSync(markdownFolder)
